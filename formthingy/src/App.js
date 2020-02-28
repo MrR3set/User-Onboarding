33a3c93cc6b2db4f2f04b3e47bf284bf3779a1c9
MrR3set <bairon.pazrobles@gmail.com>
2020-02-27 17:29:10 -0800

    mvp

--- formthingy/src/App.js
+++ formthingy/src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import UserForm from "./components/Form"
 
 function App() {
+
+  const [user, setUser] = useState([{
+    uid:1,
+    name: "some name...",
+    email:"some email...",
+    password:"some password..."
+  }]);
+  
+  const addNewUser = _user => {
+    console.log("_user")
+    // const newUser={
+    //   uid: Date.now(),
+    //   name: _user.name,
+    //   email: _user.email,
+    //   role: _user.role
+    // }
+    // setUser([...user, newUser]);
+  }
+
+
+
   return (
     <div className="App">
-      <UserForm></UserForm>
+      <UserForm addUser={addNewUser}></UserForm>
     </div>
   );
 }
--- formthingy/src/components/Form.js
+++ formthingy/src/components/Form.js
@@ -3,13 +3,14 @@ import { withFormik, Form, Field } from "formik";
 import * as yup from "yup";
 import axios from "axios";
 
-const UserForm = ({values, errors, status}) => {
-
+const UserForm = ({values, errors, status, touched}) => {
+    
     const [user, setUser] = useState([]);
 
   // listens for status changes to update animals state
   useEffect(() => {
     status && setUser(user => [...user, status]);
+
     //maybe we can send this shit somewhere?
   }, [status]);
 
@@ -20,28 +21,37 @@ const UserForm = ({values, errors, status}) => {
                 <label htmlFor="name">
                     Name
                     <Field id="name" type="text" name="name" placeholder="User name"/>
+                    {touched.name && errors.name && (
+                        <p className="errors">{errors.name}</p>
+                    )}
                 </label>
                 <label htmlFor="Email">
-                    Email
+                    Email  
                     <Field id="email" type="email" name="email" placeholder="Email"/>
+                    {touched.email && errors.email && (
+                        <p className="errors">{errors.email}</p>
+                    )}
                 </label>
                 <label htmlFor="password">
-                    Password
+                    Password  
                     <Field id="password" type="password" name="password" placeholder=""/>
+                    {touched.password && errors.password && (
+                        <p className="errors">{errors.password}</p>
+                    )}
                 </label>
                 <label className="checkbox-container">
-                    Terms of service
+                    Terms of service  
                     <Field type="checkbox" name="terms" checked={values.terms}/>
                     <span className="checkmark"/>
                 </label>
                 <button type="submit">Submit</button>
             </Form>
-            <pre>{JSON.stringify(values, null, 2)}</pre>
-            <pre>{JSON.stringify(errors, null, 2)}</pre>
+            {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
+            {/* <pre>{JSON.stringify(errors, null, 2)}</pre> */}
 
             {user.map(user=>{
                 return (
-                    <ul key={user.id}>
+                    <ul key={user.id} className="users">
                         <li>Name: {user.name}</li>
                         <li>Email: {user.email}</li>
                         <li>Password: {user.password}</li>
@@ -53,7 +63,8 @@ const UserForm = ({values, errors, status}) => {
 }
 
 const FormikUserForm = withFormik({
-    mapPropsToValues(props){
+    mapPropsToValues(props){  
+        console.log("props",props);
         return({
             name: props.name || "",
             email: props.email || "",
@@ -61,21 +72,18 @@ const FormikUserForm = withFormik({
             terms: props.terms || false
         })
     },validationSchema: yup.object().shape({
-        name: yup.string().required(),
-        email: yup.string().required(),
-        password: yup.string().required(),
-        terms: yup.string().required(),
+        name: yup.string().required("Whats your name lad? How imma supposed to knoe you?"),
+        email: yup.string().required("Bruuuuuh I ddont know your emailll?!"),
+        password: yup.string().required("Ill set it to 123 would you like that? No then put a frickin password!"),
+        terms: yup.string().required("Check that I dont want any legal thingyes on me"),
     }),
-    handleSubmit(values, { setStatus, resetForm }) {
+    handleSubmit(values, { setStatus, resetForm },props) {
         console.log("submitting");
         axios
           .post("https://reqres.in/api/users/", values)
           .then(res => {
-            console.log("success", res);
-            // sends a status update through props in Form with value as res.data content
-            setStatus(res.data);
-            //clears form inputs, from FormikBag
-            resetForm();
+              setStatus(res.data);
+              resetForm();
           })
           .catch(err => console.log(err.response));
       }
